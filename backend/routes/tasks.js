5127357a4f4891414735c15e009f1ce10fa4954e
Jiawei Li <jiawei23000@student.hamk.fi>
2025-10-11 13:16:55 +0300

    add tasks router and controller

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -6,3 +6,9 @@ tasksRouter.get('/', tasksController.getAllTasks)
 tasksRouter.get('/:id', tasksController.getTaskById)
 
 tasksRouter.post('/', tasksController.postTask)
+
+tasksRouter.put('/:id', tasksController.updateTask)
+
+tasksRouter.delete('/:id', tasksController.deleteTask)
+
+module.exports = tasksRouter
--- backend/routes/tasksController.js
+++ backend/routes/tasksController.js
@@ -3,16 +3,24 @@ const Family = require('../models/family')
 const User = require('../models/user')
 
 const getAllTasks = async (request, response) => {
-    const tasks = await Task.find({})
-    response.json(tasks) 
+    try {
+        const tasks = await Task.find({})
+        response.json(tasks) 
+    } catch (error) {
+        response.status(500).json({ error: error.message })
+    } 
 }
 
 const getTaskById = async (request, response) => {
-    const task = await Task.findById(request.param.id)
-    if (task) {
-        response.json(task)
-    } else {
-        response.status(404).json({ error: 'Task not found'})
+    try {
+        const task = await Task.findById(request.params.id)
+        if (task) {
+            response.json(task)
+        } else {
+            response.status(404).json({ error: 'Task not found'})
+        }
+    } catch (error) {
+        response.status(500).json({ error: error.message })
     }
 }
 
@@ -25,10 +33,59 @@ const postTask = async (request, response) => {
         || !request.body.assignTo)
         return response.status(400).end()
 
-    
+    try {
+        const newTask = new Task({
+            title: request.body.title,
+            description: request.body.description || '', 
+            rewardValue: request.body.rewardValue,
+            dueDate: request.body.dueDate,
+            familyId: request.body.familyId,
+            assignTo: request.body.assignTo,
+            notificationTime: request.body.notificationTime, //
+            status: 'Pending'
+        })
 
+        const savedTask = await newTask.save()
+        response.status(201).json(savedTask)
+    } catch (error) {
+        response.status(400).json({error: error.message})
+    }
+}
+
+const updateTask = async (request, response) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate (
+            request.params.id,
+            request.body,
+            {
+                new: true, //return new
+                runValidators: true //valitate schema
+            }
+        )
+        
+        if (!updatedTask){
+            return response.status(404).json({ error: 'Task not found'})
+        }
+
+        response.json(updatedTask)
+    } catch (error) {
+        response.status(400).json({ error: error.message})
+    }
+}
+
+const deleteTask = async (request, response) => {
+    try {
+        const task = await Task.findByIdAndDelete(request.params.id)
+
+        if(!task){
+            return response.status(404).json({ error: 'Task not found'})
+        }
+        response.status(204).end()
+    } catch (error) {
+        response.status(400).json({ error: error.message})
+    }
 }
 
 module.exports =  {
-    getAllTasks, getTaskById, postTask
+    getAllTasks, getTaskById, postTask, updateTask, deleteTask
 } 
