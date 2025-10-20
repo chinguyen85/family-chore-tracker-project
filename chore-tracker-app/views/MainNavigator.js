58b3edb96c48032b7012fc32b46681d3fa3a63fa
Nguyen Dinh <dinh23000@student.hamk.fi>
2025-10-20 12:01:33 +0300

    Add dummy data to test Proof Upload page

--- chore-tracker-app/views/MainNavigator.js
+++ chore-tracker-app/views/MainNavigator.js
@@ -2,13 +2,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateTask from './CreateTask';
 import TaskList from './TaskList';
 import FamilyManagement from './FamilyManagement';
+import ProofUploadScreen from './ProofUpload';
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <Tab.Navigator
-      initialRouteName="CreateTask"
+      initialRouteName="TaskList"
       screenOptions={{
         tabBarActiveTintColor: '#007AFF',
         tabBarInactiveTintColor: '#999',
@@ -31,11 +32,16 @@ const MainNavigator = () => {
         }}
       />
       <Tab.Screen
-        name="FamilyManagement"
-        component={FamilyManagement}
+        name="ProofUpload"
+        component={ProofUploadScreen}
         options={{
-          title: 'FamilyManagement',
-          tabBarLabel: 'Family',
+          title: 'Test Proof Upload',
+          tabBarLabel: 'Proof Test',
+        }}
+        // Dummy data for testing
+        initialParams={{
+          taskId: '68ee8b830c10de36bffa94b7',
+          taskTitle: 'george sleep',
         }}
       />
     </Tab.Navigator>
